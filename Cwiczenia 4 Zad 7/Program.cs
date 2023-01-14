f93f97dbb24a7a866917b9aa1b685939469bfad4
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-14 12:37:29 +0100

    Add files via upload

---
+++ Cwiczenia 4 Zad 7/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Cwiczenia_4_Zad_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            byte n = 1, m;
+
+            byte w;
+
+            Console.WriteLine("Wprowadz liczbe wierszy");
+
+            w = Convert.ToByte(Console.ReadLine());
+
+            while (n <= w)
+
+            {
+                m = 1;
+                while (m <= w)
+                {
+                    if (n == 1 | n == w | m == 1 | m == w)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                    m++;
+                }
+                Console.WriteLine();
+                n++;
+            }
+
+        }
+    }
+}
